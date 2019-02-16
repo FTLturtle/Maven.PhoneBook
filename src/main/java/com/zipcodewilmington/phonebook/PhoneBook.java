83e79e371c1fd9ffb714d2ef82e525ffef8fb95d
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-16 16:20:47 -0500

    completed all functionality of phone book and passed all unit tests

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,157 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private TreeMap<String, ArrayList<String>> phoneBookTreeMap = new TreeMap<>();
+
+
+    /**
+     * This method adds the name and array of phone phone numbers(converted to an arraylist) as a key-value
+     * mapping to the phoneBookTreeMap.
+     *
+     * @param name          to add as key
+     * @param phoneNumbers: phone phone numbers to add as values to the arraylist associated with the key
+     */
+    public void add(String name, String... phoneNumbers) {
+        ArrayList<String> phoneNumbersToAdd = new ArrayList<>(Arrays.asList(phoneNumbers));
+
+        if (hasEntry(name)) {
+            ArrayList<String> oldPhoneNumbers = phoneBookTreeMap.get(name);
+            phoneNumbersToAdd.addAll(oldPhoneNumbers);
+        }
+
+        phoneBookTreeMap.put(name, phoneNumbersToAdd);
+    }
+
+    /**
+     * This method removes a phone phone number from one of the names in the phoneBookTreeMap. If all the phone numbers
+     * associated with a name have been removed, then it gets rid of the entry altogether.
+     *
+     * @param name:         name to remove as a key
+     * @param phoneNumbers: phone numbers to add as the associated value
+     */
+    public void remove(String name, String... phoneNumbers) {
+        ArrayList<String> phoneNumbersToRemove = new ArrayList<>(Arrays.asList(phoneNumbers));
+
+        if (hasEntry(name)) {
+            ArrayList<String> oldPhoneNumbers = phoneBookTreeMap.get(name);
+            oldPhoneNumbers.removeAll(phoneNumbersToRemove);
+            phoneBookTreeMap.put(name, oldPhoneNumbers);
+
+            if (phoneBookTreeMap.get(name).isEmpty()) {
+                removeRecord(name);
+            }
+        }
+
+    }
+
+    /**
+     * void method that removes an entry from the phoneBookTreeMap
+     *
+     * @param name of entry to remove
+     */
+    public void removeRecord(String name) {
+        phoneBookTreeMap.remove(name);
+    }
+
+    /**
+     * This method returns the Value (ArrayList of String phone numbers) for which the specified key (String name) is mapped.
+     *
+     * @param name the name to look up in the phone book
+     * @return the Value (ArrayList of phone numbers) associated with the key (String name), or
+     * null if the entry is not found
+     */
+    public ArrayList<String> getArrayListFor(String name) {
+        return phoneBookTreeMap.get(name);
+    }
+
+    /**
+     * This method returns the String of phone numbers (ArrayList of String phone numbers converted to String) for which
+     * the specified key (String name) is mapped.
+     *
+     * @param name the name to look up in the phone book
+     * @return the String of phone numbers (ArrayList of phone numbers converted to String) associated with the key (String name),
+     * or an empty string if the entry is not found
+     */
+    public String getStringOfPhoneNumbersFor(String name) {
+        ArrayList<String> arrayListOfPhoneNumbers = getArrayListFor(name);
+
+        if (arrayListOfPhoneNumbers != null) {
+            return phoneBookTreeMap.get(name).toString().replaceAll("[\\[\\]]", "").replaceAll("\\n$", "");
+        }
+
+        return "";
+    }
+
+    /**
+     * This method does a reverse lookup. It looks for every Key (String name) whose Value (an ArrayList of Strings)
+     * contains the specified String phone number. It returns a String list of names associated with that phone number, or an
+     * empty string if no names are found associated with that phone number.
+     *
+     * @param phoneNumber: specified phone number used to find the names associated with it
+     * @return String names associated with specified phoneNumber
+     */
+    public String reverseLookup(String phoneNumber) {
+        StringBuilder name = new StringBuilder();
+
+        for (String key : this.phoneBookTreeMap.keySet()) {
+            if (phoneBookTreeMap.get(key).contains(phoneNumber)) {
+                name.append(key).append("\n");
+            }
+        }
+
+        return name.toString().replaceAll("[\\[\\]]", "").replaceAll("\\n$", "");
+    }
+
+    /**
+     * This is a getter method to get a string with the list of all names and phone numbers in this phone book
+     * in this sort of format:
+     * "name1 phoneNumber1, phoneNumber2, phoneNumber3\n" +
+     * "name2 phoneNumber1, phoneNumber2" +
+     * "name3 phoneNumber1, phoneNumber2, phoneNumber3"
+     *
+     * @return a string of all the names and phone numbers in the phone book
+     */
+    public String getListOfNamesAndPhoneNumbers() {
+        StringBuilder phoneBookList = new StringBuilder();
+
+        for (String key : this.phoneBookTreeMap.keySet()) {
+            phoneBookList.append(key).append(" ").append(phoneBookTreeMap.get(key)).append("\n");
+        }
+
+        return phoneBookList.toString().replaceAll("[\\[\\]]", "").replaceAll("\\n$", "");
+    }
+
+    /**
+     * returns a String list of the names contained in the phoneBookTreeMap.
+     *
+     * @return String list of names in phoneBookTreeMap
+     */
+    public String getListOfNames() {
+        StringBuilder phoneBookList = new StringBuilder();
+
+        for (String key : this.phoneBookTreeMap.keySet()) {
+            phoneBookList.append(key).append("\n");
+        }
+
+        return phoneBookList.toString().replaceAll("\\n$", "");
+    }
+
+    /**
+     * This is a private method to check if an entry is present in the phone book tree map. returns true
+     * if the entry is present, and false otherwise.
+     *
+     * @param name: the name to check
+     * @return true if the map has an entry with that name, and false otherwise
+     */
+    private boolean hasEntry(String name) {
+        return phoneBookTreeMap.containsKey(name);
+    }
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,499 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+    private PhoneBook testPhoneBook;
+
+    @Before
+    public void initialize() {
+        this.testPhoneBook = new PhoneBook();
+    }
+
+
+    @Test
+    public void addTest1() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String expectedListOfNamesAndPhoneNumbers = String.format("%s %s\n%s %s",
+                name2, Arrays.toString(phoneNumbers2).replaceAll("[\\[\\]]", ""),
+                name1, Arrays.toString(phoneNumbers1).replaceAll("[\\[\\]]", ""));
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+    @Test
+    public void addTest2() {
+        // Given
+        String name1 = "Alice";
+        String[] phoneNumbers1 = {"9870786543", "8759196783"};
+
+        String name2 = "Bob";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String expectedListOfNamesAndPhoneNumbers = String.format("%s %s\n%s %s",
+                name1, Arrays.toString(phoneNumbers1).replaceAll("[\\[\\]]", ""),
+                name2, Arrays.toString(phoneNumbers2).replaceAll("[\\[\\]]", ""));
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+    @Test
+    public void addTest3() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783", "019834759083745"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String name3 = "Abraham";
+        String[] phoneNumbers3 = {"78787656367", "987346502938475", "09164502634-9587"};
+
+        String expectedListOfNamesAndPhoneNumbers = String.format("%s %s\n%s %s\n%s %s",
+                name3, Arrays.toString(phoneNumbers3).replaceAll("[\\[\\]]", ""),
+                name2, Arrays.toString(phoneNumbers2).replaceAll("[\\[\\]]", ""),
+                name1, Arrays.toString(phoneNumbers1).replaceAll("[\\[\\]]", ""));
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.add(name3, phoneNumbers3);
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+    @Test
+    public void removeTest1() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String expectedListOfNamesAndPhoneNumbers = String.format("%s %s",
+                name2, Arrays.toString(phoneNumbers2).replaceAll("[\\[\\]]", ""));
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.remove(name1, phoneNumbers1);
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+    @Test
+    public void removeTest2() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String expectedListOfNamesAndPhoneNumbers = String.format("%s %s",
+                name1, Arrays.toString(phoneNumbers1).replaceAll("[\\[\\]]", ""));
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.remove(name2, phoneNumbers2);
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+    @Test
+    public void removeTest3() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String expectedListOfNamesAndPhoneNumbers = String.format("%s %s\n%s %s",
+                name2, Arrays.toString(phoneNumbers2).replaceAll("[\\[\\]]", ""),
+                name1, phoneNumbers1[1]);
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.remove(name1, phoneNumbers1[0]);
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+    @Test
+    public void removeTest4() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String expectedListOfNamesAndPhoneNumbers = String.format("%s %s\n%s %s",
+                name2, phoneNumbers2[0],
+                name1, Arrays.toString(phoneNumbers1).replaceAll("[\\[\\]]", ""));
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.remove(name2, phoneNumbers2[1]);
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+    @Test
+    public void removeRecordTest1() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783", "019834759083745"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String name3 = "Abraham";
+        String[] phoneNumbers3 = {"78787656367", "987346502938475", "09164502634-9587"};
+
+        String expectedListOfNamesAndPhoneNumbers = String.format("%s %s\n%s %s",
+                name3, Arrays.toString(phoneNumbers3).replaceAll("[\\[\\]]", ""),
+                name2, Arrays.toString(phoneNumbers2).replaceAll("[\\[\\]]", ""));
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.add(name3, phoneNumbers3);
+
+        testPhoneBook.removeRecord(name1);
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+    @Test
+    public void removeRecordTest2() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783", "019834759083745"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String name3 = "Abraham";
+        String[] phoneNumbers3 = {"78787656367", "987346502938475", "09164502634-9587"};
+
+        String expectedListOfNamesAndPhoneNumbers = String.format("%s %s\n%s %s",
+                name3, Arrays.toString(phoneNumbers3).replaceAll("[\\[\\]]", ""),
+                name1, Arrays.toString(phoneNumbers1).replaceAll("[\\[\\]]", ""));
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.add(name3, phoneNumbers3);
+        testPhoneBook.removeRecord(name2);
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+    @Test
+    public void removeRecordTest3() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783", "019834759083745"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String name3 = "Abraham";
+        String[] phoneNumbers3 = {"78787656367", "987346502938475", "09164502634-9587"};
+
+        String expectedListOfNamesAndPhoneNumbers = String.format("%s %s\n%s %s",
+                name2, Arrays.toString(phoneNumbers2).replaceAll("[\\[\\]]", ""),
+                name1, Arrays.toString(phoneNumbers1).replaceAll("[\\[\\]]", ""));
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.add(name3, phoneNumbers3);
+        testPhoneBook.removeRecord(name3);
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+    @Test
+    public void getArrayListForTest1() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        ArrayList<String> expectedArrayList = new ArrayList<String>(Arrays.asList(phoneNumbers2));
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        ArrayList<String> actualArrayList = testPhoneBook.getArrayListFor(name2);
+
+        // Then
+        Assert.assertEquals(expectedArrayList, actualArrayList);
+    }
+
+    @Test
+    public void getArrayListForTest2() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        ArrayList<String> expectedArrayList = null;
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        ArrayList<String> actualArrayList = testPhoneBook.getArrayListFor("Frank");
+
+        // Then
+        Assert.assertEquals(expectedArrayList, actualArrayList);
+    }
+
+    @Test
+    public void getStringOfPhoneNumbersForTest1() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String expectedStringOfPhoneNumbers = Arrays.toString(phoneNumbers1).replaceAll("[\\[\\]]", "");
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        String actualStringOfPhoneNumbers = testPhoneBook.getStringOfPhoneNumbersFor(name1);
+
+        // Then
+        Assert.assertEquals(expectedStringOfPhoneNumbers, actualStringOfPhoneNumbers);
+    }
+
+    @Test
+    public void getStringOfPhoneNumbersForTest2() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String expectedStringOfPhoneNumbers = "";
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        String actualStringOfPhoneNumbers = testPhoneBook.getStringOfPhoneNumbersFor("Frank");
+
+        // Then
+        Assert.assertEquals(expectedStringOfPhoneNumbers, actualStringOfPhoneNumbers);
+    }
+
+    @Test
+    public void reverseLookupTest1() {
+        // Given
+        String phoneNumberToLookup = "9870786543";
+
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {phoneNumberToLookup, "8759196783", "019834759083745"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String name3 = "Abraham";
+        String[] phoneNumbers3 = {"78787656367", "987346502938475", "09164502634-9587"};
+
+        String expectedReverseLookupResult = name1;
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.add(name3, phoneNumbers3);
+        String actualListOfNames = testPhoneBook.reverseLookup(phoneNumberToLookup);
+
+        // Then
+        Assert.assertEquals(expectedReverseLookupResult, actualListOfNames);
+    }
+
+    @Test
+    public void reverseLookupTest2() {
+        // Given
+        String phoneNumberToLookup = "9870786543";
+
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {phoneNumberToLookup, "8759196783", "019834759083745"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String name3 = "Abraham";
+        String[] phoneNumbers3 = {"78787656367", phoneNumberToLookup, "09164502634-9587"};
+
+        String expectedReverseLookupResult = name3 + "\n" + name1;
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.add(name3, phoneNumbers3);
+        String actualListOfNames = testPhoneBook.reverseLookup(phoneNumberToLookup);
+
+        // Then
+        Assert.assertEquals(expectedReverseLookupResult, actualListOfNames);
+    }
+
+    @Test
+    public void reverseLookupTest3() {
+        // Given
+        String phoneNumberToLookup = "0000000000";
+
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"90091723004", "8759196783", "019834759083745"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String name3 = "Abraham";
+        String[] phoneNumbers3 = {"78787656367", "86298347692", "09164502634-9587"};
+
+        String expectedReverseLookupResult = "";
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.add(name3, phoneNumbers3);
+        String actualListOfNames = testPhoneBook.reverseLookup(phoneNumberToLookup);
+
+        // Then
+        Assert.assertEquals(expectedReverseLookupResult, actualListOfNames);
+    }
+
+    @Test
+    public void getListOfNamesTest1() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783", "019834759083745"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String name3 = "Abraham";
+        String[] phoneNumbers3 = {"78787656367", "987346502938475", "09164502634-9587"};
+
+        String expectedListOfNames = String.format("%s\n%s\n%s", name3, name2, name1);
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.add(name3, phoneNumbers3);
+        String actualListOfNames = testPhoneBook.getListOfNames();
+
+        // Then
+        Assert.assertEquals(expectedListOfNames, actualListOfNames);
+    }
+
+    @Test
+    public void getListOfNamesTest2() {
+
+        String expectedListOfNames = "";
+
+        // When
+        String actualListOfNames = testPhoneBook.getListOfNames();
+
+        // Then
+        Assert.assertEquals(expectedListOfNames, actualListOfNames);
+    }
+
+    @Test
+    public void getListOfNamesAndPhoneNumbersTest1() {
+        // Given
+        String name1 = "Bob";
+        String[] phoneNumbers1 = {"9870786543", "8759196783", "019834759083745"};
+
+        String name2 = "Alice";
+        String[] phoneNumbers2 = {"38765920367", "9087378376"};
+
+        String name3 = "Abraham";
+        String[] phoneNumbers3 = {"78787656367", "987346502938475", "09164502634-9587"};
+
+        String expectedListOfNamesAndPhoneNumbers = String.format("%s %s\n%s %s\n%s %s",
+                name3, Arrays.toString(phoneNumbers3).replaceAll("[\\[\\]]", ""),
+                name2, Arrays.toString(phoneNumbers2).replaceAll("[\\[\\]]", ""),
+                name1, Arrays.toString(phoneNumbers1).replaceAll("[\\[\\]]", ""));
+
+        // When
+        testPhoneBook.add(name1, phoneNumbers1);
+        testPhoneBook.add(name2, phoneNumbers2);
+        testPhoneBook.add(name3, phoneNumbers3);
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+    @Test
+    public void getListOfNamesAndPhoneNumbersTest2() {
+        // Given
+        String expectedListOfNamesAndPhoneNumbers = "";
+
+        // When
+        String actualListOfNamesAndPhoneNumbers = testPhoneBook.getListOfNamesAndPhoneNumbers();
+
+        // Then
+        Assert.assertEquals(expectedListOfNamesAndPhoneNumbers, actualListOfNamesAndPhoneNumbers);
+    }
+
+
 }
